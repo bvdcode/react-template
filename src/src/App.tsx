53e2c90739a082708adf3032d980912400c22a33
Vadim Belov <bvdcode@users.noreply.github.com>
2025-12-07 02:03:28 -0800

    feat: update App component to include routing with BrowserRouter

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,10 +1,13 @@
+import { AppRoutes } from "./app/routes";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeContextProvider } from "./app/providers";
 
 function App() {
-
   return (
     <ThemeContextProvider>
-      <>app</>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </ThemeContextProvider>
   );
 }
