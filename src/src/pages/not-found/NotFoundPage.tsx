f5edb34f47f7943480e2d3faffe0c07a881ed9cf
Vadim Belov <git@belov.us>
2025-12-08 10:58:58 -0800

    refactor: replace Box with Paper in NotFoundPage for improved layout and styling

--- src/src/pages/not-found/NotFoundPage.tsx
+++ src/src/pages/not-found/NotFoundPage.tsx
@@ -1,17 +1,27 @@
 import { useTranslation } from "react-i18next";
-import { Box, Button, Typography, Container } from "@mui/material";
+import { Button, Typography, Container, Paper } from "@mui/material";
 
 export const NotFoundPage = () => {
   const { t } = useTranslation("notFound");
 
   return (
-    <Container maxWidth="sm">
-      <Box
+    <Container
+      maxWidth="sm"
+      sx={{
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Paper
         sx={{
-          mt: 8,
           p: 4,
           boxShadow: 3,
-          borderRadius: 2,
+          display: "flex",
+          borderRadius: 1,
+          alignItems: "stretch",
+          flexDirection: "column",
         }}
       >
         <Typography variant="h4" component="h1" gutterBottom>
@@ -23,7 +33,7 @@ export const NotFoundPage = () => {
         <Button href="/login" variant="contained" color="primary">
           {t("backButton")}
         </Button>
-      </Box>
+      </Paper>
     </Container>
   );
 };
